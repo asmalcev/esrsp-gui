6180ffdc917da01319c934ab3fb2d7b8a52a5089
Alexander <aleksmalcevrmg@gmail.com>
2022-05-24 11:42:05 +0300

    schedule request done

--- pages/api/schedule.js
+++ pages/api/schedule.js
@@ -1,5 +1,67 @@
-import data from '../../public/schedule.json';
+import sql from '../../src/db';
+import { groupBy } from '../../src/utils';
 
-export default function(req, res) {
-	res.status(200).json(data);
+const getSchedule = async (id) => {
+	const schedule = await sql`
+	select
+		D.name as name,
+		T.fullname as teacher,
+		(
+			select STRING_AGG(SG.name, ', ') from studentgroup as SG
+			where id in (
+				select studentgroupid from flow
+				where id = C.flowid
+			)
+		) as groups,
+		C.place,
+		C.classday,
+		C.classnumber
+	from class as C
+	join discipline as D on D.id = C.disciplineid
+	join teacher as T on T.id = C.teacherid
+	where teacherid = ${id}
+	order by C.classday, C.classnumber;
+	`;
+
+	const classnumberTimes = [
+		{
+			timeStart: '9:00',
+			timeEnd: '10:30',
+		},
+		{
+			timeStart: '10:50',
+			timeEnd: '12:20',
+		},
+		{
+			timeStart: '12:40',
+			timeEnd: '14:10',
+		},
+		{
+			timeStart: '14:55',
+			timeEnd: '16:25',
+		},
+		{
+			timeStart: '16:45',
+			timeEnd: '18:15',
+		}
+	]
+
+	const scheduleGuiVersion = schedule.map(v => ({
+		members: v.groups,
+
+		...v,
+		...classnumberTimes[v.classnumber - 1]
+	}));
+
+	const grouped = groupBy(scheduleGuiVersion, 'classday');
+
+	const keys = Object.keys(grouped);
+	const values = Object.values(grouped)
+	return values.map((v, i) => ({order: Number(keys[i]), lessons: v}));
+}
+
+export default async (req, res) => {
+	const result = await getSchedule(3);
+
+	res.status(200).json(result);
 }
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,32 +1,6 @@
 import sql from '../../src/db';
 import { groupBy } from '../../src/utils';
 
-const getSchedule = async () => {
-	const schedule = await sql`
-	select
-		D.name as discipline,
-		T.fullname as teacher,
-		(
-			select STRING_AGG(SG.name, ', ') from studentgroup as SG
-			where id in (
-				select studentgroupid from flow
-				where id = C.flowid
-			)
-		) as groups,
-		C.place,
-		C.classday,
-		C.classnumber
-	from class as C
-	join discipline as D on D.id = C.disciplineid
-	join teacher as T on T.id = C.teacherid
-	where teacherid = 1
-	order by C.classday, C.classnumber;
-	`;
-	return groupBy(schedule, 'classday');
-}
-
 export default async function(req, res) {
-	const result = await getSchedule();
-
-	res.status(200).json(result);
+	res.status(200).json({test: 'test'});
 }
