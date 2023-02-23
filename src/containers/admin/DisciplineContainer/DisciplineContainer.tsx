b9bfc44ef3a642f69d5553e3f9a47a2a26fd7c4e
qzerrty <qzerrty@protonmail.com>
2023-02-24 00:33:22 +0300

    refactor: DisciplineContainer и StudentContainer на AdminEditGenerator

--- src/containers/admin/DisciplineContainer/DisciplineContainer.tsx
+++ src/containers/admin/DisciplineContainer/DisciplineContainer.tsx
@@ -1,64 +1,24 @@
-import { Typography } from '@mui/material';
-import { useSnackbar } from 'notistack';
-import { useState } from 'react';
-import { Discipline } from '../../../backend.types';
-import Button from '../../../common/Button';
-import InputBox from '../../../common/InputBox';
-import TextField from '../../../common/TextField';
-import { WideLayout } from '../../Layout';
+import AdminEditGenerator from '../AdminEditGenerator';
 
-const DisciplineContainer = ({ data }: { data: Discipline }) => {
-	const [name, setName] = useState<string>(data.name);
-	const { enqueueSnackbar } = useSnackbar();
-
-	const onSave = async (e) => {
-		e.preventDefault();
-
-		if (name.length === 0) {
-			enqueueSnackbar(`Error: discipline name cannot be empty`, {
-				variant: 'error',
-			});
-			return;
-		}
-
-		const body: Discipline = {
-			id: data.id,
-			name,
-		};
-
-		const res = await fetch(`/api/schedule/discipline/${data.id}`, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
+const StudentContainer = AdminEditGenerator({
+	title: 'Управление дисциплинами',
+	fields: {
+		id: {
+			disabled: true,
+			type: 'string',
+		},
+		name: {
+			type: 'string',
+			validate: (v: string) => {
+				if (v.length > 0) {
+					return { isOk: true, error: '' };
+				}
+				return { isOk: false, error: 'name cannot be empty' };
 			},
-			body: JSON.stringify(body),
-		});
+			prepare: (v: string) => v.trim(),
+		},
+	},
+	fetchUrl: '/api/schedule/discipline/',
+});
 
-		if (res.status === 200) {
-			enqueueSnackbar(`Success: saved`, { variant: 'success' });
-		} else {
-			enqueueSnackbar(`Error ${res.status}`, { variant: 'error' });
-		}
-	};
-
-	return (
-		<WideLayout>
-			<Typography variant="h2">Управление дисциплинами</Typography>
-			<form onSubmit={onSave}>
-				<InputBox sx={{ width: 'auto' }}>
-					<TextField label="id" value={data.id} disabled />
-				</InputBox>
-				<InputBox sx={{ width: 'auto' }}>
-					<TextField label="name" value={name} onChange={setName} />
-				</InputBox>
-				<InputBox sx={{ width: 'auto' }}>
-					<Button onClick={onSave} type="submit">
-						Сохранить
-					</Button>
-				</InputBox>
-			</form>
-		</WideLayout>
-	);
-};
-
-export default DisciplineContainer;
+export default StudentContainer;
--- src/containers/admin/StudentContainer/StudentContainer.tsx
+++ src/containers/admin/StudentContainer/StudentContainer.tsx
@@ -1,69 +1,34 @@
-import { Typography } from '@mui/material';
-import { useSnackbar } from 'notistack';
-import { useState } from 'react';
-import { Student } from '../../../backend.types';
-import Button from '../../../common/Button';
-import InputBox from '../../../common/InputBox';
-import TextField from '../../../common/TextField';
-import { WideLayout } from '../../Layout';
+import AdminEditGenerator from '../AdminEditGenerator';
 
-const StudentContainer = ({ data }: { data: Student }) => {
-	const [fullname, setFullname] = useState<string>(data.fullname);
-	const [recordBook, setRecordBook] = useState<string>(data.recordBook);
-	const { enqueueSnackbar } = useSnackbar();
-
-	const onSave = async (e) => {
-		e.preventDefault();
-
-		if (fullname.length === 0) {
-			enqueueSnackbar(`Error: discipline name cannot be empty`, {
-				variant: 'error',
-			});
-			return;
-		}
-
-		const body: Student = {
-			id: data.id,
-			fullname,
-			recordBook,
-		};
-
-		const res = await fetch(`/api/schedule/student/${data.id}`, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
+const StudentContainer = AdminEditGenerator({
+	title: 'Управление студентами',
+	fields: {
+		id: {
+			disabled: true,
+			type: 'string',
+		},
+		fullname: {
+			type: 'string',
+			validate: (v: string) => {
+				if (v.length > 0) {
+					return { isOk: true, error: '' };
+				}
+				return { isOk: false, error: 'fullname cannot be empty' };
 			},
-			body: JSON.stringify(body),
-		});
-
-		if (res.status === 200) {
-			enqueueSnackbar(`Success: saved`, { variant: 'success' });
-		} else {
-			enqueueSnackbar(`Error ${res.status}`, { variant: 'error' });
+			prepare: (v: string) => v.trim(),
+		},
+		recordBook: {
+			type: 'string',
+			validate: (v: string) => {
+				if (v.length > 0) {
+					return { isOk: true, error: '' };
+				}
+				return { isOk: false, error: 'recordBook cannot be empty' };
+			},
+			prepare: (v: string) => v.trim(),
 		}
-	};
-
-	return (
-		<WideLayout>
-			<Typography variant="h2">Управление дисциплинами</Typography>
-			<form onSubmit={onSave}>
-				<InputBox sx={{ width: 'auto' }}>
-					<TextField label="id" value={data.id} disabled />
-				</InputBox>
-				<InputBox sx={{ width: 'auto' }}>
-					<TextField label="name" value={fullname} onChange={setFullname} />
-				</InputBox>
-				<InputBox sx={{ width: 'auto' }}>
-					<TextField label="name" value={recordBook} onChange={setRecordBook} />
-				</InputBox>
-				<InputBox sx={{ width: 'auto' }}>
-					<Button onClick={onSave} type="submit">
-						Сохранить
-					</Button>
-				</InputBox>
-			</form>
-		</WideLayout>
-	);
-};
+	},
+	fetchUrl: '/api/roles/student/',
+});
 
 export default StudentContainer;
